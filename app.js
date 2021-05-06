49f99040f2c40d37c791e87ebe551fd6d677c0fe
Nicolas Golob <nicolasgolob@gmail.com>
2021-05-06 09:29:18 +0200

    corrective actions on the buildingDate function

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 
 const api = {
     key:"    ",
-    //attention à ne pas laisser la clef durant l'envoie
     base:"https://api.openweathermap.org/data/2.5/"
 }
 
@@ -31,16 +30,17 @@ function displayWeatherData(weather){
     let temperature  = document.querySelector('.current .temperature');
     temperature.innerText=`${Math.round(weather.main.temp)}`;
     //math round obviously to round the resulting measurement 
-    let date = document.querySelector('.location.date');
+    let now = new Date();
+    let date = document.querySelector('.location .date');
     date.innerText=buildingDate(now);
 
     function buildingDate(dateArg){
-        let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         let day = days[dateArg.getDay()];
-        let date = dateArg.getDay();
-        let month = months[dateArg.getMouth()];
-        let year = dateArg.getYear();
+        let date = dateArg.getDate();
+        let month = months[dateArg.getMonth()];
+        let year = dateArg.getFullYear();
         return`${day} ${date} ${month} ${year}`
     }
 }
