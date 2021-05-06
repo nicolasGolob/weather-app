9032c9f7f894031ebf50a4582114cb136fc6d4d4
Nicolas Golob <nicolasgolob@gmail.com>
2021-05-06 13:24:46 +0200

    icon and css adjustment

--- app.js
+++ app.js
@@ -16,23 +16,37 @@ function setQuery(event){
 }
 
 function getWeatherData(query){
-    fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
-    .then(weather =>{
-        return weather.json();
+    window.fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
+    .then(weathers =>{
+    //once you've got the api back you take what it sent back to you
+    //-> weathers = the name of the object given to it -> corresponds to a large object that the api sends back to us
+        return weathers.json();
+    //convert me this object to json
     }).then(displayWeatherData);
+    //then -> promise displayWeatherData in order to dispatch and use the data with this function
 }
 
-function displayWeatherData(weather){
-    console.log(weather);
+function displayWeatherData(weathers){
+    console.log(weathers);
     let city = document.querySelector('.location .city');
-    city.innerText=`${weather.name}, ${weather.sys.country}`;
+    city.innerText=`${weathers.name}, ${weathers.sys.country}`;
     // src:openweathmap : sys -> Internal parameter -> country code 
-    let temperature  = document.querySelector('.current .temperature');
-    temperature.innerText=`${Math.round(weather.main.temp)}`;
-    //math round obviously to round the resulting measurement 
     let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText=buildingDate(now);
+
+    let temperature  = document.querySelector('.current .temperature');
+    temperature.innerText=`${Math.round(weathers.main.temp)}`;
+    //math round obviously to round the resulting measurement 
+    
+    let weatherElement = weathers.weather[0].icon;
+    $('.weather-icon').attr('src', 'http://openweathermap.org/img/wn/'+ weatherElement +'.png')
+
+    let lowestTemp = document.querySelector('.current .lowest');
+    lowestTemp.innerText = `Min : ${weathers.main.temp_min}°c`;
+
+    let highestTemp = document.querySelector('.current .highest');
+    highestTemp.innerText = `Max : ${weathers.main.temp_max}°c`;
 
     function buildingDate(dateArg){
         let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
